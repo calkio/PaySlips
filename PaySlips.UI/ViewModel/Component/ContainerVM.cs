c86a06e51ac5fd1200ab40ef3eacfe67e04b3ebd
DONYAaaa <danil.surov.03@mail.ru>
2025-02-16 19:43:53 +0700

    контейнер

---
+++ PaySlips.UI/ViewModel/Component/ContainerVM.cs
@@ -0,0 +1,37 @@
+﻿using PaySlips.UI.Infastructure.Command;
+using PaySlips.UI.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PaySlips.UI.ViewModel.Component
+{
+    internal class ContainerVM : BaseVM
+    {
+        #region ПОЛЯ
+
+        #endregion
+
+        #region СВОЙСТВА
+
+        private string _title;
+        public string Title { get => _title; set => Set(ref _title, value); }
+        
+        private IEnumerable<BaseVM> _items;
+        public IEnumerable<BaseVM> Items { get => _items; set => Set(ref _items, value); }
+
+
+        #endregion
+
+
+        public ContainerVM(string title, IEnumerable<BaseVM> items)
+        {
+            Title = title;
+            Items = items;
+        }
+    }
+
+}
