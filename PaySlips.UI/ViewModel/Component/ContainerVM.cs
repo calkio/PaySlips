2636b6e622d02f2353fbabd80d11082d3992dd47
DONYAaaa <danil.surov.03@mail.ru>
2025-02-18 19:06:59 +0700

    merge

--- PaySlips.UI/ViewModel/Component/ContainerVM.cs
+++ PaySlips.UI/ViewModel/Component/ContainerVM.cs
@@ -33,18 +33,6 @@ namespace PaySlips.UI.ViewModel.Component
         private BaseVM _searchLineVM;
         public BaseVM SearchLineVM { get => _searchLineVM; set => Set(ref _searchLineVM, value); }
 
-        private BaseVM _directoryVM;
-        public BaseVM DirectoryVM { get => _directoryVM; set => Set(ref _directoryVM, value); }
-
-        private BaseVM _titleButtonVM;
-        public BaseVM TitleButtonVM { get => _titleButtonVM; set => Set(ref _titleButtonVM, value); }
-
-        private BaseVM _dateButtonVM;
-        public BaseVM DateButtonVM { get => _dateButtonVM; set => Set(ref _dateButtonVM, value); }
-
-        private BaseVM _fileButtonArchiveVM;
-        public BaseVM FileButtonArchiveVM { get => _fileButtonArchiveVM; set => Set(ref _fileButtonArchiveVM, value); }
-
         #endregion
 
 
@@ -64,10 +52,7 @@ namespace PaySlips.UI.ViewModel.Component
         {
             Title = title;
             Items = items;
-            DirectoryVM = directoryVM;
             SearchLineVM = searchLineVM;
-            TitleButtonVM = titleButtonVM;
-            DateButtonVM = dateButtonVM;
         }
     }
 
